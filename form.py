28385563df3d2f97e1c24fbb457a79f66d4a532d
Rick Bounce Development <rickbounce.dev@gmail.com>
2019-01-25 02:37:54 +0200

    Сейчас есть Хендлеры и второе окно вызывается просто по кнопке логин - пока логики нет

--- form.py
+++ form.py
@@ -1,37 +1,75 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel,QLineEdit, QPushButton,QGridLayout
+from PyQt5.QtCore import  pyqtSlot
+from PyQt5.QtWidgets import QMainWindow,QApplication,QLineEdit,QLabel,QTextEdit,QGridLayout,QPushButton,QHBoxLayout,QVBoxLayout,QFormLayout
 
 
-class Example(QWidget):
+class NewWindow(QMainWindow):
+    NumGridRows = 3
+    NumButtons = 4
+
     def __init__(self):
-        super().__init__()
+        super(NewWindow, self).__init__()
         self.initUI()
 
-
     def initUI(self):
-        title = QLabel('Login')
-        author = QLabel('Password')
+        b1 = QPushButton("Button1")
+        b2 = QPushButton("Button2")
 
-        titleEdit = QLineEdit()
-        authorEdit = QLineEdit()
-        grid = QGridLayout()
-        grid.setSpacing(10)
-        grid.addWidget(title, 1, 0)
-        grid.addWidget(titleEdit, 1, 1)
-        grid.addWidget(author, 2, 0)
-        grid.addWidget(authorEdit, 2, 1)
-        self.setLayout(grid)
-        self.btnq = QPushButton("Login", self)
-        self.setGeometry(300, 300, 450, 400)
-        self.setWindowTitle('Seif')
+        self.lab = QLabel("Введите код подтверждения который был отправлен вам на почту",self)
+        self.subm = QPushButton('Submit',self)
+        self.textbox = QLineEdit(self)
+        self.textbox.move(10, 10)
+        self.textbox.resize(480, 280)
+        self.lab.move(50,300)
+        self.lab.resize(400,50)
+        self.subm.move(200,360)
+        mainLayout = QVBoxLayout()
+        mainLayout.addWidget(b1)
+        mainLayout.addWidget(b2)
+        self.setLayout(mainLayout)
+        self.setGeometry(500, 500, 500, 500)
+        self.setWindowTitle('Absolute')
         self.show()
 
-        self.btnq.clicked.connect(self.ButtonClicked)
+class Example(QMainWindow):
+    ##################################
+    def getx(self): return self.__addwin
+    def setx(self, value): self.__addwin = value
+    def delx(self): del self.__addwin
+    addwin = property(getx, setx, delx, "Свойство 'addwin'.")
 
-    def ButtonClicked(self):
-        print("hi")
+    def getx(self): return self.__addwin2
+    def setx(self, value): self.__addwin2 = value
+    def delx(self): del self.__addwin2
+    addwin2 = property(getx, setx, delx, "Свойство 'addwin2'.")
+    ################################################
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
 
-if __name__ == '__main__':
+    def initUI(self):
+        self.bqt = QPushButton('Login',self)
+        self.login = QLabel('Login',self)
+        self.password = QLabel('Password',self)
+        self.loginIn = QLineEdit(self)
+        self.passIn = QLineEdit(self)
+        self.login.move(250,10)
+        self.password.move(240,100)
+        self.loginIn.move(220,50)
+        self.passIn.move(220,140)
+        self.bqt.move(220,200)
+        self.setGeometry(300, 300, 550, 300)
+        self.setWindowTitle('Review')
+        self.show()
+        self.bqt.clicked.connect(self.addwindow)
+    @pyqtSlot()
+    def addwindow(self):
+        self.addwin = NewWindow()
+
+def main():
     app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
